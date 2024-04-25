4a5e390f9af941a2c3893279595c2229b2f4ffe8
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-25 20:43:40 +0530

    adding the modals for the task management app

---
+++ modals/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: Boolean, enum: ['pending', 'completed'], default: 'pending' },
+    priority: { type: String, enum: ['low', 'medium', 'high'], required: true },
+    date: { type: Date, required: true },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}, { timestamps: true })
+
+const Task = mongoose.model('task', taskSchema);
+
+module.exports = Task;
