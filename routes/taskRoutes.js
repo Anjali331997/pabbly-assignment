4902e3a78ce288869b83e4697f0d49a58ec32eff
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-25 20:46:06 +0530

    adding the routes

---
+++ routes/taskRoutes.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/authenticationMiddleware')
+const Task = require('../modals/Task')
+
+router.get('/test', (req, res) => {
+    res.send("task routes are working")
+})
+
+//curd task for authenticated users
+
+//create a task
+router.post('/', auth, async (req, res) => {
+
+    try {
+        //description,complted from req.body
+        //try to get owner:req.user._id
+        const task = new Task({
+            ...req.body,
+            owner: req.user._id
+        })
+        await task.save();
+        res.status(200).json({
+            task,
+            meassage: "New task added successfully"
+        })
+    }
+    catch (error) {
+        res.status(400).send({ error: error.message })
+        console.log(error.message)
+    }
+})
+
+//get the tasks
+router.get('/', auth, async (req, res) => {
+    try {
+        const tasks = await Task.find({ owner: req.user._id })
+        res.status(200).send({ tasks })
+    } catch (error) {
+        res.status(404).send({ error: error.meassage })
+        console.log(error.message)
+    }
+})
+
+//get a task by id
+router.get('/:id', auth, async (req, res) => {
+    const taskid = req.params.id
+    try {
+
+        const task = await Task.findOne({ _id: taskid, owner: req.user._id })
+        if (!task) {
+            res.status(404).json({ message: "Task not found" })
+        }
+        else {
+            res.status(200).json({ task })
+        }
+
+    } catch (error) {
+        res.status(404).send({ error: error.meassage })
+        console.log(error.message)
+    }
+})
+
+//update a task - can updated the description and completed field
+router.patch('/:id', auth, async (req, res) => {
+    const taskid = req.params.id
+    const updates = Object.keys(req.body);
+    // console.log(req.body);
+    const allowedUpdated = ['description', 'completed'];
+    {
+        //The every() method executes a function for each array element.
+        //The every() method returns true if the function returns true for all elements.
+        //The every() method returns false if the function returns false for one element.
+
+        //we are checking if every element in the updates array in present in alloweupdated arrays
+    }
+    const isValidOperation = updates.every(ele => allowedUpdated.includes(ele));
+
+    if (!isValidOperation) {
+        res.status(404).send({ Error: "Invalid updates" })
+    }
+    try {
+        const task = await Task.findOneAndUpdate({ _id:taskid }, req.body,{new:true})
+        console.log(task)
+        if (!task) {
+            res.status(404).json({ Error: "Task not found" })
+        }
+
+        res.status(200).json({ task, message: "Task successfullt updatedd" })
+
+    } catch (error) {
+        res.status(500).send({ error: error.meassage })
+        console.log(error.message)
+    }
+})
+
+//delete a task 
+router.delete('/:id',auth, async (req,res)=>{
+    const taskid = req.params.id;
+    try {
+        const task = await Task.findOneAndDelete({ _id:taskid ,owner:req.user._id})
+        console.log(task)
+        if (!task) {
+            res.status(404).json({ Error: "Task not found" })
+        }
+
+        res.status(200).json({ task, message: "Task successfullt deleted" })
+
+    } catch (error) {
+        res.status(500).send({ error: error.meassage })
+        console.log(error.message)
+    }
+})
+
+
+
+module.exports = router
---
+++ routes/userRoutes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const User = require('../modals/User')
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+router.get('/', (req, res) => {
+    res.send("USer routes are working")
+})
+
+//sign-up
+router.post('/register', async (req, res) => {
+
+    try {
+        const { name, email, password } = req.body;
+        const new_user = new User({ name, email, password });
+        await new_user.save();
+        res.status(201).send({ new_user, message: "User created successfully" })
+    }
+    catch (err) {
+        res.status(400).send({ error: err })
+        console.log(err.message)
+    }
+})
+
+//login-up
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({email:email})
+        console.log(user)
+        if (!user) {
+            throw new Error("Unable to login, Invalid credentials")
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            throw new Error("Unable to login, Invalid credentials")
+        }
+
+        const token = jwt.sign({
+            _id: user._id.toString()
+        }, process.env.JWT_SECRET_KEY)
+        
+        res.send({ user, token });
+    
+    } catch (err) {
+        res.status(400).send({ error: err })
+    }
+})
+
+
+
+module.exports = router
