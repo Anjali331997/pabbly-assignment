c95f6de1fb8df90a97cd5fb8f3ed1e0a33cea59d
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-25 20:46:44 +0530

    authentication middleware

---
+++ middleware/authenticationMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../modals/User');
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+        const user = await UserModel.findOne({ _id: decoded._id });
+        if (!user) {
+            throw new Error('Unable to find access, Please login');
+        }
+
+        req.user = user;
+        req.token = token;
+        next();
+    }
+    catch (err) {
+        res.status(401).send({ error: err.message })
+    }
+}
+
+module.exports =  authMiddleware ;
