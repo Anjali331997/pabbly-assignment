c5ec74e3425d8fd378e75a2a140165dc81f57eed
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-28 05:43:13 +0530

    adding cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userRouter = require('./routes/userRoutes')
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-app.use(cros({
+app.use(cors({
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
